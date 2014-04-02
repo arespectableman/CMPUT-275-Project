3454a20f394037d6c87dbb3fb10615ad4338c50c
Prateek Srivastava <f2prateek@gmail.com>
2014-04-02 02:06:19 -0600

    Better looking map

--- client/RouteMapClient/app/src/main/java/com/f2prateek/routemapclient/app/ui/VenuesMapFragment.java
+++ client/RouteMapClient/app/src/main/java/com/f2prateek/routemapclient/app/ui/VenuesMapFragment.java
@@ -1,14 +1,20 @@
 package com.f2prateek.routemapclient.app.ui;
 
+import android.content.Context;
 import android.location.Location;
 import android.widget.Toast;
 import com.f2prateek.routemapclient.app.FoursquareFactory;
+import com.f2prateek.routemapclient.app.R;
 import com.f2prateek.routemapclient.app.model.foursquare.FoursquareVenue;
 import com.f2prateek.routemapclient.app.model.foursquare.VenuesResponse;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.android.clustering.view.DefaultClusterRenderer;
 import pl.charmas.android.reactivelocation.ReactiveLocationProvider;
 import retrofit.Callback;
 import retrofit.RetrofitError;
@@ -17,14 +23,21 @@ import rx.util.functions.Action1;
 
 public class VenuesMapFragment extends MapFragment implements Callback<VenuesResponse> {
 
+  ClusterManager<FoursquareVenue> clusterManager;
+
   @Override public void onStart() {
     super.onStart();
+
+    getMap().setMyLocationEnabled(true);
+    getMap().setIndoorEnabled(true);
+    getMap().getUiSettings().setAllGesturesEnabled(true);
+
     ReactiveLocationProvider locationProvider = new ReactiveLocationProvider(getActivity());
     locationProvider.getLastKnownLocation().subscribe(new Action1<Location>() {
       @Override
       public void call(Location location) {
-        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(
-            new LatLng(location.getLatitude(), location.getLongitude()), 10));
+        centerMap(location);
+
         FoursquareFactory.getInstance()
             .venues(location.getLatitude(), location.getLongitude(), VenuesMapFragment.this);
       }
@@ -32,14 +45,36 @@ public class VenuesMapFragment extends MapFragment implements Callback<VenuesRes
   }
 
   @Override public void success(VenuesResponse response, Response raw) {
-    ClusterManager clusterManager = new ClusterManager<FoursquareVenue>(getActivity(), getMap());
-    clusterManager.addItems(response.venues);
+    clusterManager = new ClusterManager<FoursquareVenue>(getActivity(), getMap());
+    clusterManager.setRenderer(new VenueRenderer(getActivity(), getMap(), clusterManager));
 
     getMap().setOnCameraChangeListener(clusterManager);
     getMap().setOnMarkerClickListener(clusterManager);
+
+    clusterManager.addItems(response.venues);
+    clusterManager.cluster();
+  }
+
+  private void centerMap(Location location) {
+    LatLng moveTo = new LatLng(location.getLatitude(), location.getLongitude());
+    getMap().animateCamera(CameraUpdateFactory.newLatLngZoom(moveTo, 10));
   }
 
   @Override public void failure(RetrofitError error) {
     Toast.makeText(getActivity(), "An unknown error occurred.", Toast.LENGTH_LONG).show();
+  }
+
+  static class VenueRenderer extends DefaultClusterRenderer<FoursquareVenue> {
+
+    public VenueRenderer(Context context, GoogleMap map,
+        ClusterManager<FoursquareVenue> clusterManager) {
+      super(context, map, clusterManager);
+    }
+
+    @Override
+    protected void onBeforeClusterItemRendered(FoursquareVenue venue, MarkerOptions markerOptions) {
+      markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location_marker))
+          .title(venue.name);
+    }
   }
 }
